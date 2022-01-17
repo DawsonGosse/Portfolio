0f2e7e4d92c1850299ff3a012f37eafe1660d39e
dawsonGosse <dgosse5@my.bcit.ca>
2022-01-16 20:16:20 -0800

    Navbar & darkmode implementation completed

---
+++ comps/Icons/Moon/index.js
@@ -0,0 +1,22 @@
+import * as React from "react"
+
+const Moon = ({onclick, display}) => (
+  <svg
+    width={32}
+    height={32}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    onClick={() => onclick()}
+    display={display}
+
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M31.897 22.065a1.27 1.27 0 0 0-.315-1.44 1.352 1.352 0 0 0-1.494-.218 12.81 12.81 0 0 1-5.557 1.256c-6.957 0-12.597-5.497-12.597-12.278 0-2.746.922-5.277 2.482-7.321.33-.432.35-1.018.052-1.471a1.341 1.341 0 0 0-1.395-.564C5.6 1.601 0 8.078 0 15.833 0 24.762 7.426 32 16.587 32c6.898 0 12.808-4.102 15.31-9.935Z"
+      fill="#5C5769"
+    />
+  </svg>
+)
+
+export default Moon
---
+++ comps/NavBar/index.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import styled from 'styled-components';
+import { useTheme } from '../../utils/provider'
+import { themes } from '../../utils/variables'
+import LinkedIn from '../Icons/LinkedIn';
+import Instagram from '../Icons/Instagram';
+import GitHub from '../Icons/Github';
+import Moon from '../Icons/Moon';
+import Sun from '../Icons/Sun';
+
+const Container = styled.div`
+    width:100%;
+    height:56px;
+    background-color:${props=>props.bgcolor};
+    display:flex;
+    box-shadow: ${props=>props.shadow}
+`;
+const Section2 = styled.div`
+    display:flex;
+    flex:1;
+    justify-content:center;
+    align-items:center;
+`;
+const Section3 = styled.div`
+    display:flex;
+    flex:1;
+    justify-content:flex-end;
+    align-items:center;
+    margin-right:52px;
+`;
+const Section1 = styled.div`
+    display:flex;
+    flex:1;
+    justify-content:flex-start;
+    align-items:center;
+    margin-left:52px;
+`;
+
+const Links = styled.a`
+    color:${props=>props.color};
+    margin:15px;
+
+    &:hover {
+        color:#CA6444;
+    }
+`;
+
+const NavBar = ({
+    onButtonClick=()=>{}
+
+}) =>{
+    const {theme} = useTheme();
+
+    return <Container bgcolor={themes[theme].body} shadow={themes[theme].shadow}>
+        <Section1>
+            <Moon onclick={onButtonClick} display={themes[theme].displayMoon}></Moon>
+            <Sun  onclick={onButtonClick} display={themes[theme].displaySun}></Sun>
+        </Section1>
+        <Section2>
+            <Links href='/' color={themes[theme].color}>Home</Links>
+            <Links href='/projects.js' color={themes[theme].color}>Projects</Links>
+            <Links href='/contact.js' color={themes[theme].color}>Contact</Links>
+        </Section2>
+        <Section3>
+            <LinkedIn color={themes[theme].color}></LinkedIn>
+            <Instagram color={themes[theme].color}></Instagram>
+            <GitHub color={themes[theme].color}></GitHub>
+        </Section3>
+    </Container>
+}
+
+export default NavBar;
